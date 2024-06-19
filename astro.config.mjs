eba43f5173f7cdea2f35ac0bba6878b7b8740274
Romario <romario.jara.ch@gmail.com>
2024-06-19 18:44:16 -0500

    add @astrojs/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
   output: 'server',
   devToolbar: {
     enabled: false
-  }
+  },
+  adapter: cloudflare()
 });
