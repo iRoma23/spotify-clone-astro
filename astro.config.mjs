82d50ee6fa43730fd85995d90d0b23255598301d
Romario <romario.jara.ch@gmail.com>
2024-06-23 01:47:02 -0500

    vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
+// import cloudflare from "@astrojs/cloudflare";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  adapter: cloudflare()
+  adapter: vercel()
 });
