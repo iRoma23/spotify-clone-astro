37f6014b351110ba6e34845d803bc4eb9e7545a0
Romario <romario.jara.ch@gmail.com>
2024-06-23 02:17:36 -0500

    uninstall @astrojs/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
-// import cloudflare from "@astrojs/cloudflare";
 
 import vercel from "@astrojs/vercel/serverless";
 
